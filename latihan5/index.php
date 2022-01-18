bf3b7a5f7448ca62e18e73d0cac89ef6f1ea88d2
Ridhoanandaa <88362323+Ridhoanandaa@users.noreply.github.com>
2022-01-18 15:07:13 +0700

    init
    inisialisasi

---
+++ latihan5/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style/myStyle.css" />
+    <title>index</title>
+  </head>
+  <body>
+    <!-- bg lisensi-->
+    <!-- <a href='https://www.freepik.com/vectors/design'>Design vector created by rawpixel.com - www.freepik.com</a> -->
+    <!-- lisensi icon -->
+    <!-- <a href="https://www.flaticon.com/free-icons/office-worker" title="office worker icons">Office worker icons created by Freepik - Flaticon</a> -->
+    <!-- <a href="https://www.flaticon.com/free-icons/admin" title="admin icons">Admin icons created by Freepik - Flaticon</a> -->
+    <div class="container">
+      <div class="top">
+        <div class="logo">
+          <p>logo dinkes</p>
+        </div>
+        <div class="right">
+          <h1>Tittle</h1>
+        </div>
+        <div class="left">
+          <a href="">Logout </a>
+        </div>
+      </div>
+      <div class="mid">
+        <div class="card-button card-button" data-tilt>
+          <h3>ADMIN</h3>
+          <p>klik Lanjut Untuk Pilih Data Admin</p>
+          <img src="bg/admin.png" alt="admin" /><br />
+          <a href="">Lanjut</a>
+        </div>
+        <div class="card-button card-button" data-tilt="">
+          <h3>JABATAN</h3>
+          <p>klik Lanjut Untuk Pilih Data Jabatan</p>
+          <img src="bg/position.png" alt="jabatan" /><br />
+          <a href="">Lanjut</a>
+        </div>
+        <div class="card-button card" data-tilt="">
+          <h3>PEGAWAI</h3>
+          <p>klik Lanjut Untuk Pilih Data Pegawai</p>
+          <img src="bg/worker.png" alt="office worker" /><br />
+          <a href="">Lanjut</a>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="footer">
+          <p>Made By anak Magang polnep 2021/2022</p>
+        </div>
+      </div>
+    </div>
+  </body>
+  <script src="script/vanilla-tilt.min.js"></script>
+</html>
---
+++ latihan5/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style/myStyle.css" />
+    <title>login</title>
+  </head>
+  <body>
+    <div class="container-login">
+      <div class="container-form">
+        <p class="logo-dinkes">logo dinkes</p>
+        <h3>SILAHKAN LOGIN</h3>
+        <form action="">
+          <label for="user">username</label><br />
+          <input type="text" class="form form-user" placeholder="ketikan disini" /><br />
+          <label for="pass">Password</label><br />
+          <input type="password" class="form form-pass" placeholder="ketikan disini" /><br />
+          <input type="submit" value="cancel" class="button-form" />
+          <input type="submit" value="submit" class="button-form" />
+        </form>
+        <div class="cop">
+          <p>@copyright 2022</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
